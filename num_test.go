3fc09a346255d0d67743cbcce7202bfbd62f9758
conbanwa <35380552+conbanwa@users.noreply.github.com>
2024-03-22 11:36:13 +0800

    FuzzParseFloat64

--- num_test.go
+++ num_test.go
@@ -2,12 +2,86 @@ package num
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 	"testing"
 
 	"github.com/conbanwa/logs"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/exp/constraints"
 )
+
+func FuzzParseFloat64(f *testing.F) {
+	var (
+		a int
+		b uint
+		c int8
+		d int16
+		e int32
+		j float32 = float32(math.NaN())
+		k float64 = math.NaN()
+		g string  = "NaN"
+	)
+	f.Add(a, b, c, d, e, j, k, g)
+	f.Fuzz(func(t *testing.T, a int, b uint, c int8, d int16, e int32, j float32, k float64, g string) {
+		parseFloat(t, a)
+		parseFloat(t, b)
+		parseFloat(t, c)
+		parseFloat(t, d)
+		parseFloat(t, e)
+		parseFloat(t, j)
+		parseFloat(t, k)
+		parseFloat(t, g)
+	})
+}
+func TestParseFloat64(t *testing.T) {
+	tests := []struct {
+		name  string
+		a     any
+		wantF float64
+	}{
+		{
+			name:  "1",
+			a:     1,
+			wantF: 1,
+		}, {
+			name:  "2",
+			a:     "1",
+			wantF: 1,
+		}, {
+			name:  "3",
+			a:     math.NaN(),
+			wantF: math.NaN(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			parseFloat(t, tt.a)
+		})
+	}
+}
+func parseFloat(t *testing.T, a any) {
+	f, err := ParseFloat64(a)
+	if err != nil {
+		return
+	}
+	switch a.(type) {
+	case string:
+		sf, err := strconv.ParseFloat(a.(string), 64)
+		if math.IsNaN(f) && math.IsNaN(sf) {
+			return
+		}
+		assert.Equal(t, sf, f, err)
+		return
+	case float32:
+		if math.IsNaN(f) && math.IsNaN(float64(a.(float32))) {
+			return
+		}
+		assert.Equal(t, float32(f), a)
+		return
+	}
+	assert.Equalf(t, fmt.Sprint(a), fmt.Sprint(f), "ParseFloat64(%v)", a)
+}
 
 func BenchmarkFloatToString(b *testing.B) {
 	for i := 0; i < b.N; i++ {
