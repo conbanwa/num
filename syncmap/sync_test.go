06a5630a3d340ccec96151417b422832a0a76554
conbanwa <35380552+conbanwa@users.noreply.github.com>
2024-03-26 21:56:12 +0800

    Update sync_test.go

--- syncmap/sync_test.go
+++ syncmap/sync_test.go
@@ -12,10 +12,17 @@ func TestSyncMapValue(t *testing.T) {
 	var sm sync.Map
 	assert.Equal(t, 0, Len(&sm))
 	sm.Store(1, []int{2})
-	assert.Equal(t, 1, Len(&sm))
-	t.Run("tt.name", func(t *testing.T) {
-		if gotRes := ValueStrict[[]int](&sm, 1); !reflect.DeepEqual(gotRes, []int{2}) || !reflect.DeepEqual(Value(&sm, 1), []int{2}) {
+	sm.Store(3, "55")
+	assert.Equal(t, 2, Len(&sm))
+	t.Run("0", func(t *testing.T) {
+		if gotRes := ValueStrict[[]int](&sm, 1); !reflect.DeepEqual(gotRes, []int{2}) || !reflect.DeepEqual(Value[[]int](&sm, 1), []int{2}) {
 			t.Errorf("ValueStrict() = %v, want %v", gotRes, []int{})
+		}
+		if gotRes := Value[[]int](&sm, 11); !reflect.DeepEqual(gotRes, []int(nil)) {
+			t.Errorf("ValueStrict() = %v, want %v", gotRes, []int(nil))
+		}
+		if gotRes := ValueStrict[string](&sm, 3); !reflect.DeepEqual(gotRes, "55") {
+			t.Errorf("ValueStrict() = %v, want %v", gotRes, "")
 		}
 	})
 }
